e48fe8d7a15dab1ad7460c9a02c36aa359f5e934
jobegood49 <jonathan.s.nicolas@gmail.com>
2020-04-03 22:34:30 -0700

    Amend data attributes

--- the-dom/4-data-attributes/data-attributes.js
+++ the-dom/4-data-attributes/data-attributes.js
@@ -9,14 +9,14 @@ console.log(pic);
 // The required alt attribute specifies an alternate text for an
 // image, if the image cannot be displayed.
 
-//  Step-2 can set this manually
+//Step-2 can set the alt attribute this manually
 
 pic.alt = "this is a random pic";
 
 console.log(pic.alt);
 // We can also set the width of the picture
 
-pic.width = 300;
+pic.width = 500;
 
 // Now if you instert an error in  src="https://source.unsplash.com/random/600x600"
 // and reload, no image will appear but the alt text attribute
