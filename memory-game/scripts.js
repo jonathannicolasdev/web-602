32f96865cc5d46ca02614c2de212d63e523ec7b5
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-05-30 07:57:48 -0700

    Upadata may 30

--- memory-game/scripts.js
+++ memory-game/scripts.js
@@ -1,37 +1,11 @@
-// Let's make he card flip when we click it
-// First lets make a list of all out memory card elements
+console.log("we are working on a game");
 
 const cards = document.querySelectorAll(".memory-card");
-
-// When a player clicks a card we must know if its the first or second card that is
-// being clicked
-
-let hasFlippedCard = false;
-let firstCard, secondCard;
+console.log(cards);
 
 function flipCard() {
   this.classList.toggle("flip");
-  // console.log("hello");
-  // console.log(this);
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-  } else {
-    hasFlippedCard = false;
-    secondCard = this;
-    console.log({ firstCard, secondCard });
-    // now that we have both cards that we clicked we need to check if these two cards match
-    // we can add a data attribute to our element
-    console.log(firstCard.dataset.pokemon);
-    console.log(secondCard.dataset.pokemon);
-  }
-
-  // console.log({ hasFlippedCard, firstCard });
 }
-
-// Loop through that list of cards and to each one of the cards we will attach
-// an event listener, we will listen for a click event and when that event is fired
-// we will execute a function names flipcard
 
 cards.forEach(function (card) {
   card.addEventListener("click", flipCard);
