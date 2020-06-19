a14fc39c3c416a639d45980711fde1ca2a3a2f6e
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2020-06-19 16:06:01 -0700

    Add reverstring aldo

---
+++ wrap-up/algorithm/solution.js
@@ -0,0 +1,15 @@
+function reverseString(string) {
+  // you can only write your code here!
+  let reverseString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseString += string[i];
+  }
+  return reverseString;
+}
+
+// TEST CASES
+console.log(reverseString("Hello World and Coders")); // sredoC dna dlroW olleH
+console.log(reverseString("John Doe")); // eoD nhoJ
+console.log(reverseString("I am a bookworm")); // mrowkoob a ma I
+console.log(reverseString("Coding is my hobby")); // ybboh ym si gnidoC
+console.log(reverseString("Super")); // repuS
